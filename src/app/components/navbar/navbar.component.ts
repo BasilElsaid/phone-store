901171978481143ff04bbb05cd869e0a02505a1a
Basil Elsaid <basil_elsaid@outlook.com>
2025-05-04 12:40:25 +0200

    improvements in navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { DarkModeService } from '../../services/dark-mode-service.service';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
