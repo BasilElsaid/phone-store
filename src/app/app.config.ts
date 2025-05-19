403bf3a66276fa283b0d6626c7809197a2db7251
Basil Elsaid <basil_elsaid@outlook.com>
2025-05-19 21:12:09 +0200

    bugs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,13 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()]
+    provideHttpClient(),
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+  ]
 };
