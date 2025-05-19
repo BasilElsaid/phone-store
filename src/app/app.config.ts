158e42177706c4a2dcbd30045a0cfedc6d99939d
Basil Elsaid <basil_elsaid@outlook.com>
2025-05-19 21:39:05 +0200

    Use production build in build script

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,11 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
-    { provide: LocationStrategy, useClass: HashLocationStrategy },
   ]
 };
