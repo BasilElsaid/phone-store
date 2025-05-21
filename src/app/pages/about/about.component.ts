0529792197aaa6cd625f95043c5d0670c639d9ab
Basil Elsaid <basil_elsaid@outlook.com>
2025-05-21 14:03:51 +0200

    updated about page

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { DarkModeBackgroundDirective } from '../../directives/dark-mode-background.directive';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [DarkModeBackgroundDirective],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
